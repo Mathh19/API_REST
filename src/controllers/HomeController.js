723e8585b1e3c1601170540bb58c8c2142a8e5b3
Matheus Freitas <mf635097@gmail.com>
2022-02-16 13:46:13 -0300

    Feito rotas de alunos

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,16 +1,6 @@
-import Aluno from '../models/Aluno';
-
 class HomeController {
   async index(req, res) {
-    const novoAluno = await Aluno.create({
-      nome: 'Luiz',
-      sobrenome: 'Otavio',
-      email: 'otavio@gmail.com',
-      idade: 45,
-      peso: 90,
-      altura: 1.80,
-    });
-    res.json(novoAluno);
+    res.json('Index');
   }
 }
 
